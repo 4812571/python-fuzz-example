3a4c9b3232feef8a3c4f4019df8bb8ef2998ffe5
Dylan <57853417+4812571@users.noreply.github.com>
2022-10-05 08:01:16 -0400

    initial commit

---
+++ fuzzme.py
@@ -0,0 +1,9 @@
+from distutils.log import error
+
+def fuzzme_fn(input):
+    if input[0] == 'b':
+        if input[1] == 'u':
+            if input[2] == 'g':
+                error('you got it!')
+    
+    return 0
---
+++ main.py
@@ -0,0 +1,3 @@
+import fuzzme
+
+fuzzme.fuzzme_fn('bu1g')
